25bbc3aff2d60bbbde67a55dc4c51a62c6f7fa9a
10adn <10adnanummer@gmail.com>
2026-05-04 17:39:38 +0530

    Fix database sslmode issue

--- perfume_site/settings.py
+++ perfume_site/settings.py
@@ -120,7 +120,6 @@ if HAS_DJ_DATABASE_URL and os.getenv('DATABASE_URL'):
     DATABASES['default'] = dj_database_url.config(
         default=os.getenv('DATABASE_URL'),
         conn_max_age=600,
-        ssl_require=not DEBUG,
     )
 
 # ========================
